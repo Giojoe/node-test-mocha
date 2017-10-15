ba706136eaef982726f0e65d7b3cd3103c6f0605
JoeGio <jaturon.kumo@gmail.com>
2017-10-15 13:49:28 +0700

    add test express app

--- server/server.js
+++ server/server.js
@@ -4,7 +4,26 @@ var app = express();
 const port = process.env.PORT || 3000 ; 
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+  res.status(404).send({
+    error: 'Page not found.',
+    name: 'Todo App v1.0.0'
+  });
 });
 
+app.get('/users', (req, res) => {
+  res.send([{
+    name: 'Jame',
+    age: 23
+  },{
+    name: 'Joe',
+    age: 22
+  }, {
+    name: 'Nes',
+    age: 23
+  }]);
+});
+// GET /users
+//Give users a name prop , age prop
+
 app.listen(3000);
+module.exports.app = app;
