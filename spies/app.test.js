1fb65a50fa8d03b37b437022c7f3e81a3e9dbf83
JoeGio <jaturon.kumo@gmail.com>
2017-10-15 14:29:18 +0700

    add spy test

---
+++ spies/app.test.js
@@ -0,0 +1,27 @@
+const expect = require('expect');
+const rewire = require('rewire');
+
+var app = rewire('./app');
+
+
+describe('App Express spy', () => {
+  var db = {
+    saveUser: expect.createSpy()
+  };
+  app.__set__('db', db);
+
+  it('should call the spy correctly', () => {
+    var spy = expect.createSpy();
+    spy('Jaturon', 22);
+    expect(spy).toHaveBeenCalledWith('Jaturon',22);
+  });
+
+  it('should call saveUser with user object', () => {
+    var email = 'qwerty@gmail.com'
+    var password = '12345'
+
+    app.handleSignUp(email, password);
+    expect(db.saveUser).toHaveBeenCalledWith({email, password})
+
+  });
+});
---
+++ spies/db.js
@@ -0,0 +1,4 @@
+module.exports.saveUser = (user) => {
+  console.log('Save a user', user)
+};
+
